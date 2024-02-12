f769bd5144b6c114e8f5578cf197a7cec57580b2
Nulljy <jjy951122@naver.com>
2024-02-12 15:40:44 +0900

    before using virtual keyword on Base class file extends02.cpp

--- Extends/Extends/extends02.cpp
+++ Extends/Extends/extends02.cpp
@@ -4,7 +4,7 @@ class Base {
 	std::string s;
 
 public:
-	Base() : s("기반") { std::cout << "기반 클래스" << std::endl; }
+	Base() : s("기반") { std::cout << "기반 클래스의 what()" << std::endl; }
 
 	void what() { std::cout << s << std::endl; }
 };
@@ -14,7 +14,7 @@ class Derived : public Base {
 
 public:
 	Derived() : Base(), s("파생") {
-		std::cout << "파생 클래스" << std::endl;
+		std::cout << "파생 클래스의 what()" << std::endl;
 
 		what();
 	}
@@ -24,6 +24,15 @@ public:
 };
 
 int main() {
-	Derived a;
+	Base p;
+	Derived c;
+
+	Base* p_c = &c;
+	Base* p_p = &p;
+
+	std::cout << " == 실제 객체는 BASE == \n";
+	p_p->what();
+	std::cout << " == 실제 객체는 Derived == \n";
+	p_c->what();
 	return 0;
 }
