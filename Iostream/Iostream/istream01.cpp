59c24ecdf7b9124ad3c03cb3274d46351e5af4a6
Nulljy <jjy951122@naver.com>
2024-02-14 17:38:28 +0900

    using cin.fail, clear, ignore function in iostream01.cpp

--- Iostream/Iostream/istream01.cpp
+++ Iostream/Iostream/istream01.cpp
@@ -4,10 +4,17 @@
 using namespace std;
 int main() {
 	int t;
-	std::cin >> t;
-	std::cout << "fail 비트가 켜진 상태이므로, 입력x\n";
-	std::string s;
-	std::cin >> s;
+	while (true) {
+		std::cin >> t;
+		if (std::cin.fail()) { // fail함수
+			std::cout << "제대로 입력해주세요" << std::endl;
+			std::cin.clear(); // 플래그 초기화 함수
+			std::cin.ignore(100, '\n'); // ignore함수
+		}
+		if (t == 0) break;
+		std::cout << "입력 :: " << t << std::endl;
+	}
+	
 	//while (std::cin >> t) {
 	//	std::cout << "입력 : " << t << std::endl; // 'c'입력시 failbit이 on되어서 버퍼의 'c\n'은 손대지 않고 리턴해버린다.
 	//	if (t == 0) break; // 이유는 t가 int 타입인데 'c'는 char 형태라서
