e7d0af79758afd58f91f338734ec79aa0a3b13e2
Nulljy <jjy951122@naver.com>
2024-02-14 17:35:30 +0900

    failbit on iostream 01

--- Iostream/Iostream/istream01.cpp
+++ Iostream/Iostream/istream01.cpp
@@ -4,10 +4,13 @@
 using namespace std;
 int main() {
 	int t;
-	while (true) {
-		std::cin >> t;
-		std::cout << "입력 : " << t << std::endl; // 'c'입력시 failbit이 on되어서 버퍼의 'c\n'은 손대지 않고 리턴해버린다.
-		if (t == 0) break; // 이유는 t가 int 타입인데 'c'는 char 형태라서
-	}
+	std::cin >> t;
+	std::cout << "fail 비트가 켜진 상태이므로, 입력x\n";
+	std::string s;
+	std::cin >> s;
+	//while (std::cin >> t) {
+	//	std::cout << "입력 : " << t << std::endl; // 'c'입력시 failbit이 on되어서 버퍼의 'c\n'은 손대지 않고 리턴해버린다.
+	//	if (t == 0) break; // 이유는 t가 int 타입인데 'c'는 char 형태라서
+	//}
 	return 0;
 }
