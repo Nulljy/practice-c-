3e9a049ed87a9165d73709a0b3647f3d47278cca
Nulljy <jjy951122@naver.com>
2024-02-14 15:45:00 +0900

    study iostream. using file istream01.cpp put char type in int string. the reason is study failbit

---
+++ Iostream/Iostream/istream01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+	int t;
+	while (true) {
+		std::cin >> t;
+		std::cout << "입력 : " << t << std::endl; // 'c'입력시 failbit이 on되어서 버퍼의 'c\n'은 손대지 않고 리턴해버린다.
+		if (t == 0) break; // 이유는 t가 int 타입인데 'c'는 char 형태라서
+	}
+	return 0;
+}
