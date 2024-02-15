c872c2a34c5a34ba4c3f641cee47d4a3caf8a48f
Nulljy <jjy951122@naver.com>
2024-02-15 22:55:08 +0900

    make to_number func using istringstream

--- Iostream/Iostream/stringstream.cpp
+++ Iostream/Iostream/stringstream.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <sstream>
 
-int main() {
-	std::istringstream ss("123"); // istringstream은 마치 문자열을 하나의 스트림이라 생각하게 해주는 가상화 장치
-	int x;
-	ss >> x;
+double to_number(std::string s) {
+	std::istringstream ss(s);
+	double x;
 
-	std::cout << "입력 받은 데이터 :: " << x << std::endl;
+	ss >> x;
+	return x;
+}
+
+int main() {
+	std::cout << "변환:: 1 + 2 = " << to_number("1") + to_number("2") << std::endl;
 
 	return 0;
 }
