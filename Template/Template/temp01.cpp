90b6887670f57556cbd91be07ef364b92e8a5496
Nulljy <jjy951122@naver.com>
2024-02-17 09:50:25 +0900

    템플릿으로 배열 출력 // 하는 이유 : 매번 std::array<T, SIZE>에서 SIZE를 바꿔주기 힘드니 템플릿 사용

--- Template/Template/temp01.cpp
+++ Template/Template/temp01.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <array>
 
-template <typename T, int num> // 타입이 아닌 템플릿 인자
-T add_num(T t) {
-	return t + num;
+template <typename T> // 타입이 아닌 템플릿 인자
+void print_array(const T& arr) {
+	for (int i = 0; i < arr.size(); i++) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
 }
+
 int main() {
-	int a = add_num<int, 5>(4);
-	std::cout << a << std::endl;
+	std::array<int, 5> arr = { 1, 2, 3, 4, 5 };
+	std::array<int, 7> arr1 = { 1, 2, 3, 4, 5, 6, 7 };
+
+	print_array(arr);
+	print_array(arr1);
+
+
 	return 0;
 }
