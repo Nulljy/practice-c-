a7ebd622ab1ff143efd68fe48b9685675ce12b54
Nulljy <jjy951122@naver.com>
2024-02-17 14:33:41 +0900

    after using default constructor

--- Template/Template/temp01.cpp
+++ Template/Template/temp01.cpp
@@ -4,12 +4,12 @@
 
 
 
-template <typename T> // before using default constructor
+template <typename T> // after using default constructor
 struct Compare {
-	bool operator()(const T& a, const T& b) const { return a < b; }
+	bool operator()(const T& a, const T& b) const { return a < b; } // a < b  a가 b보다 작은게 트루면 트루
 };
 
-template<typename T, typename Comp>
+template<typename T, typename Comp = Compare<T>>
 T Min(T a, T b) {
 	Comp comp;
 	if (comp(a, b)) {
@@ -23,5 +23,5 @@ int main() {
 	char a1 = 'c';
 	char a2 = 'a';
 
-	std::cout << "min : " << Min<char, Compare<char>>(a1, a2) << std::endl;
+	std::cout << "min : " << Min(a, b) << std::endl;
 }
