66b935d3ee555fd08769f4397551396bd277ebce
Nulljy <jjy951122@naver.com>
2024-02-23 16:08:56 +0900

    greater_comp 함수객체

--- Algorithm/Algorithm/sort01.cpp
+++ Algorithm/Algorithm/sort01.cpp
@@ -11,6 +11,14 @@ void print(Iter begin, Iter end) {
 	}
 	std::cout << std::endl;
 }
+//struct int_compare { // 함수 객체
+//	bool operator()(const int& a, const int& b) const { return a > b; } // 이 경우 int, string 만들어야함
+//};
+template<typename T>
+struct greater_comp {
+	bool operator()(const T& a, const T& b) const { return a > b; }// 그래서 greater_comp 사용, operator>가 있어야함
+};
+
 
 int main() {
 	std::vector<int> vec;
@@ -23,7 +31,8 @@ int main() {
 	vec.push_back(2);
 
 	print(vec.begin(), vec.end());
-	std::sort(vec.begin(), vec.end());
+	//std::sort(vec.begin(), vec.end(), int_compare()); // sort02.cpp
+	std::sort(vec.begin(), vec.end(), greater_comp<int>()); // sort02.cpp
 
 	print(vec.begin(), vec.end());
 
