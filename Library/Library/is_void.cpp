3f9f857954711808aac8109eb065eeb3eb9fc7af
Nulljy <jjy951122@naver.com>
2024-03-10 21:35:54 +0900

    템플릿 메타함수

---
+++ Library/Library/is_void.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+template <typename T>
+struct is_void {
+	static constexpr bool value = false;
+};
+
+template <>
+struct is_void<void> {
+	static constexpr bool value = true;
+};
+
+template<typename T>
+void tell_type() {
+	if (is_void<T>::value) {
+		std::cout << "T는 void! \n";
+	}
+	else {
+		std::cout << "T는 void가 아니다 \n";
+	}
+}
+
+int main() {
+	tell_type<int>();
+	tell_type<void>();
+}
